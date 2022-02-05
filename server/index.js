1fcf618471a79a89c56de326f7daf9afa645a692
rileylarsonpro <riley.larson.pro@gmail.com>
2022-02-05 11:54:22 -0700

    Embedded custom express server into next application

--- server/index.js
+++ server/index.js
@@ -1,45 +1,9 @@
-/*const express = require('express')
-const app = express()
+const { app, server } = require('./server')
 const port = 1334
 
-const cors = require(`cors`)
-
-app.use(cors())
-app.use(express.json())
-app.use(express.text())
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/', (req, res) => {
-    const type = req.headers['content-type']
-    if (type !== 'text/plain' && type !== 'application/json'){
-        res.status(400).send("Unknown Content Type")
-    }
-    else {
-        res.send(req.body)
-    }
-})
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-}) */
-
-/*
-TODO: New index.js
-const app require('./server')
- app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
+app.prepare().then(() => {
+  server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`Ready on http://localhost:${port}`)
   })
-*/ 
-const {server} = require('./server')
-const port = 1334
-
-async function run () {
-  const app = await server()
-  app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-  })
-}
-run().catch(console.error)
+})
--- server/server.js
+++ server/server.js
@@ -1,52 +1,52 @@
+// Define App
 const Enforcer = require('openapi-enforcer')
 const EnforcerMiddleware = require('openapi-enforcer-middleware')
 const express = require('express')
 const path = require('path')
 
-exports.server = async function () {
-  const app = express()
-  
-  // Any paths defined in your openapi.yml will validate and parse the request
-  // before it calls your route code.
-  const openapiPath = path.resolve(__dirname, '../openAPIDocumentation.yml')
-  const enforcer = await Enforcer(openapiPath)
-  const enforcerMiddleware = EnforcerMiddleware(enforcer) // can now accpet enforcer promice 
-  app.use(enforcerMiddleware.init())
-  //TODO:   app.use(enforcerMiddleware.init({baseUrl: '/api'}))
+const next = require('next')
+const bodyParser = require('body-parser')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+module.exports.app = app
+
+// Define Server
+const server = express()
+const openapiPath = path.resolve(__dirname, '../openAPIDocumentation.yml')
+const enforcerPromice = Enforcer(openapiPath)
+const enforcerMiddleware = EnforcerMiddleware(enforcerPromice) 
 
 
-  // Catch errors
-  enforcerMiddleware.on('error', err => {
+server.use(bodyParser.json())
+
+// Logging Requests
+server.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next()
+})
+
+// Use openAPIEnforcer on all /server requests
+server.use('/server/*', enforcerMiddleware.init({baseUrl: '/server'}))
+
+// Put custom routes here
+
+// Use mock middleware 
+server.use('/server/*', enforcerMiddleware.mock())
+
+
+enforcerMiddleware.on('error', err => {
     console.error(err)
     process.exit(1)
-  }) 
-  
-  // If your openapi.yml file defines this path then this path will only
-  // execute when the request is valid otherwise it will send back a 400
-  // with a message describing why the request was invalid.
-  app.get('/api/users/:userId', (req, res) => {
-    // OLD WAY: get the userId as a string
-    const userIdOldWay = req.params.userId  
-  
-    // BETTER WAY: get the userID as the type defined in your openapi.yml file
-    const userId = req.enforcer.params.userId
-  
-    // ... do some processing
-  
-    // validate, serialize, and send a response that follows your openapi.yml file
-    res.enforcer.send({
-      userId,
-  
-      // The date object will serialize to the correct format, according to your
-      // openapi.yml file.  Most likely this will be either the openapi format
-      // `date` or `date-time`.
-      birthDate: new Date('2000-01-01') 
-    })
-    
-  })
-  // add fallback mocking middleware here
-  app.use(enforcerMiddleware.mock())
+}) 
 
-  return app
-}
+// All other request are handled by NEXT
+server.get('*', (req, res) => {
+    return handle(req, res)
+}) 
+
+module.exports.server = server
+
 
